1c46d179b8d687d7dc263d225be493917b61f324
ravi <ravisankar86885@gmail.com>
2022-10-28 11:35:15 +0530

    day one work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import HomePage from "./pages/HomePage";
+import CoinsPage from "./pages/CoinsPage";
+
+import { styled } from "@mui/system";
 
 function App() {
+  // const useStyles = makeStyles({
+  //   App: {
+  //     backgroundColor: "#14161a",
+  //     color: "white",
+  //     minHeight: "100vh",
+  //   },
+  // });
+  const MyComponent = styled("div")({
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <MyComponent>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/coins/:id" element={<CoinsPage />} />
+        </Routes>
+      </MyComponent>
+    </BrowserRouter>
   );
 }
 
